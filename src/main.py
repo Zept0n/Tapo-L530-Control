305aa56a1f119a06e26f20ed7ab64cd67c0f1136
Zept0n <104791727+Zept0n@users.noreply.github.com>
2023-10-04 17:29:52 +0300

    removed unnecessary code

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import importlib
+import src.config as config
+import asyncio
+from src.light_control import LightControl
+
+async def main():
+    importlib.reload(config)
+    try:
+        light_control = LightControl(config.USERNAME,config.PASSWORD,config.IP)
+        await light_control.login()
+    except Exception as e:
+        print(f"Login failed: {e}")
+    try:
+        await light_control.run()
+    except Exception as e:
+        print("Run failed: {e}")
+
+
+
